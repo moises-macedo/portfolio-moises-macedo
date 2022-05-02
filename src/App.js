a71620cccf11d350a6d2928c3d0266e0a690481a
Moisés Alex de Castro Macedo <91707010+moises-macedo@users.noreply.github.com>
2022-05-02 16:19:46 -0300

    ...

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./app.css"
 function App() {
   return (
     <div>
-      <h1 className="title--preLoarders"> Site em Construção... <PreLoarders/></h1>
+      <h1 className="title--preLoarders"> Site em construção... <PreLoarders/></h1>
       <p className="title--sorry">Desculpe-me pelo transtorno</p>
       
     </div>

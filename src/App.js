c84ca2f9f9b53469afef8019ca6e85fbd1366a70
Moisés Alex de Castro Macedo <91707010+moises-macedo@users.noreply.github.com>
2022-05-02 16:13:17 -0300

    começo de projeto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import PreLoarders from "./Components/PreLoaders";
+import "./app.css"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="title--preLoarders"> Site em Construção... <PreLoarders/></h1>
+      <p className="title--sorry">Desculpe-me pelo transtorno</p>
+      
     </div>
   );
 }
---
+++ src/Components/PreLoaders/index.jsx
@@ -0,0 +1,17 @@
+import "../../Assests/Styles/Css/preLoarders.css"
+
+const PreLoarders = () => {
+
+    return (
+        <div className="loader">
+            <div className="loader-square"></div>
+            <div className="loader-square"></div>
+            <div className="loader-square"></div>
+            <div className="loader-square"></div>
+            <div className="loader-square"></div>
+            <div className="loader-square"></div>
+            <div className="loader-square"></div>
+        </div>
+    )
+}
+export default PreLoarders

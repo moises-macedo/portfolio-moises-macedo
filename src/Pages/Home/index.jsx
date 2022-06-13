c0d6fe683824e41e90a05af4a7b2fde2f52ff43d
Moisés Alex de Castro Macedo <91707010+moises-macedo@users.noreply.github.com>
2022-06-13 10:24:04 -0300

    fix: Script About Me
    feito inclusa de musica no componente

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,16 +1,11 @@
-import Header from "../../Components/Header";
 import Welcome from "../../Components/Welcome";
-import Jobs from "../../Components/Jobs";
 import "../../Assets/Styles/Css/pages.css"
-import Salutation from "../../Components/ Salutation";
-import { useState } from "react";
 
 const Home = () => {
-    const [isActiveJobs, setIsActiveJobs] = useState(false)
+    
     return (
         <div className="container--home">            
-            <Welcome />
-            
+            <Welcome />            
         </div>
     )
 }
